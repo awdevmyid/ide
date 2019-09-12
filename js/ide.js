a412be5b7654e4eceaafba4239b15c7cee57b7cc
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2019-09-13 00:02:35 +0200

    Adapt for Stemalica.

--- js/ide.js
+++ js/ide.js
@@ -3,22 +3,16 @@ var wait = localStorageGetItem("wait") || false;
 var pbUrl = "https://pb.judge0.com";
 var check_timeout = 200;
 
-var blinkStatusLine = ((localStorageGetItem("blink") || "true") === "true");
-
 var layout;
 
 var sourceEditor;
 var stdinEditor;
 var stdoutEditor;
-var stderrEditor;
-var compileOutputEditor;
-var sandboxMessageEditor;
 
 var isEditorDirty = false;
 var currentLanguageId;
 
 var $selectLanguage;
-var $insertTemplateBtn;
 var $runBtn;
 var $statusLine;
 
@@ -39,7 +33,7 @@ var layoutConfig = {
         content: [{
             type: "component",
             componentName: "source",
-            title: "SOURCE",
+            title: "Program",
             isClosable: false,
             componentState: {
                 readOnly: false
@@ -51,7 +45,7 @@ var layoutConfig = {
                 content: [{
                     type: "component",
                     componentName: "stdin",
-                    title: "STDIN",
+                    title: "Ulaz",
                     isClosable: false,
                     componentState: {
                         readOnly: false
@@ -62,31 +56,7 @@ var layoutConfig = {
                 content: [{
                         type: "component",
                         componentName: "stdout",
-                        title: "STDOUT",
-                        isClosable: false,
-                        componentState: {
-                            readOnly: true
-                        }
-                    }, {
-                        type: "component",
-                        componentName: "stderr",
-                        title: "STDERR",
-                        isClosable: false,
-                        componentState: {
-                            readOnly: true
-                        }
-                    }, {
-                        type: "component",
-                        componentName: "compile output",
-                        title: "COMPILE OUTPUT",
-                        isClosable: false,
-                        componentState: {
-                            readOnly: true
-                        }
-                    }, {
-                        type: "component",
-                        componentName: "sandbox message",
-                        title: "SANDBOX MESSAGE",
+                        title: "Izlaz",
                         isClosable: false,
                         componentState: {
                             readOnly: true
@@ -151,51 +121,12 @@ function handleResult(data) {
     var status = data.status;
     var stdout = decode(data.stdout);
     var stderr = decode(data.stderr);
-    var compile_output = decode(data.compile_output);
-    var sandbox_message = decode(data.message);
     var time = (data.time === null ? "-" : data.time + "s");
     var memory = (data.memory === null ? "-" : data.memory + "KB");
 
     $statusLine.html(`${status.description}, ${time}, ${memory}`);
 
-    if (blinkStatusLine) {
-        $statusLine.addClass("blink");
-        setTimeout(function() {
-            blinkStatusLine = false;
-            localStorageSetItem("blink", "false");
-            $statusLine.removeClass("blink");
-        }, 3000);
-    }
-
-    stdoutEditor.setValue(stdout);
-    stderrEditor.setValue(stderr);
-    compileOutputEditor.setValue(compile_output);
-    sandboxMessageEditor.setValue(sandbox_message);
-
-    if (stdout !== "") {
-        var dot = document.getElementById("stdout-dot");
-        if (!dot.parentElement.classList.contains("lm_active")) {
-            dot.hidden = false;
-        }
-    }
-    if (stderr !== "") {
-        var dot = document.getElementById("stderr-dot");
-        if (!dot.parentElement.classList.contains("lm_active")) {
-            dot.hidden = false;
-        }
-    }
-    if (compile_output !== "") {
-        var dot = document.getElementById("compile-output-dot");
-        if (!dot.parentElement.classList.contains("lm_active")) {
-            dot.hidden = false;
-        }
-    }
-    if (sandbox_message !== "") {
-        var dot = document.getElementById("sandbox-message-dot");
-        if (!dot.parentElement.classList.contains("lm_active")) {
-            dot.hidden = false;
-        }
-    }
+    stdoutEditor.setValue(stdout || stderr);
 
     $runBtn.removeClass("loading");
 }
@@ -209,10 +140,7 @@ function save() {
         source_code: encode(sourceEditor.getValue()),
         language_id: $selectLanguage.val(),
         stdin: encode(stdinEditor.getValue()),
-        stdout: encode(stdoutEditor.getValue()),
-        stderr: encode(stderrEditor.getValue()),
-        compile_output: encode(compileOutputEditor.getValue()),
-        sandbox_message: encode(stderrEditor.getValue())
+        stdout: encode(stdoutEditor.getValue())
     });
     var filename = "judge0-ide.json";
     var data = {
@@ -229,8 +157,8 @@ function save() {
         },
         data: data,
         success: function (data, textStatus, jqXHR) {
-            if (getIdFromURI() != data["long"]) {
-                window.history.replaceState(null, null, location.origin + location.pathname + "?" + data["long"]);
+            if (getIdFromURI() != data["short"]) {
+                window.history.replaceState(null, null, location.origin + location.pathname + "?" + data["short"]);
             }
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -253,13 +181,10 @@ function loadSavedSource() {
             $selectLanguage.dropdown("set selected", data["language_id"]);
             stdinEditor.setValue(decode(data["stdin"]));
             stdoutEditor.setValue(decode(data["stdout"]));
-            stderrEditor.setValue(decode(data["stderr"]));
-            compileOutputEditor.setValue(decode(data["compile_output"]));
-            sandboxMessageEditor.setValue(decode(data["sandbox_message"]));
             changeEditorLanguage();
         },
         error: function (jqXHR, textStatus, errorThrown) {
-            showError("Not Found", "Code not found!");
+            showError("Greška", "Program nije pronađen!");
             window.history.replaceState(null, null, location.origin + location.pathname);
             loadRandomLanguage();
         }
@@ -268,21 +193,13 @@ function loadSavedSource() {
 
 function run() {
     if (sourceEditor.getValue().trim() === "") {
-        showError("Error", "Source code can't be empty!");
+        showError("Greška", "Program ne može biti prazan!");
         return;
     } else {
         $runBtn.addClass("loading");
     }
 
-    document.getElementById("stdout-dot").hidden = true;
-    document.getElementById("stderr-dot").hidden = true;
-    document.getElementById("compile-output-dot").hidden = true;
-    document.getElementById("sandbox-message-dot").hidden = true;
-
     stdoutEditor.setValue("");
-    stderrEditor.setValue("");
-    compileOutputEditor.setValue("");
-    sandboxMessageEditor.setValue("");
 
     var sourceValue = encode(sourceEditor.getValue());
     var stdinValue = encode(stdinEditor.getValue());
@@ -330,17 +247,16 @@ function fetchSubmission(submission_token) {
 
 function changeEditorLanguage() {
     monaco.editor.setModelLanguage(sourceEditor.getModel(), $selectLanguage.find(":selected").attr("mode"));
-    $(".lm_title")[0].innerText = fileNames[currentLanguageId];
 }
 
 function insertTemplate() {
+    currentLanguageId = parseInt($selectLanguage.val());
     sourceEditor.setValue(sources[currentLanguageId]);
     changeEditorLanguage();
 }
 
 function loadRandomLanguage() {
     $selectLanguage.dropdown("set selected", Math.floor(Math.random() * $selectLanguage[0].length));
-    currentLanguageId = parseInt($selectLanguage.val());
     insertTemplate();
 }
 
@@ -353,18 +269,10 @@ $(document).ready(function () {
 
     $selectLanguage = $("#select-language");
     $selectLanguage.change(function (e) {
-        currentLanguageId = parseInt($selectLanguage.val());
         if (!isEditorDirty) {
             insertTemplate();
         } else {
             changeEditorLanguage();
-        }
-    });
-
-    $insertTemplateBtn = $("#insert-template-btn");
-    $insertTemplateBtn.click(function (e) {
-        if (isEditorDirty && confirm("Are you sure? Your current changes will be lost.")) {
-            insertTemplate();
         }
     });
 
@@ -380,22 +288,6 @@ $(document).ready(function () {
         if (keyCode == 120) { // F9
             e.preventDefault();
             run();
-        } else if (keyCode == 119) { // F8
-            e.preventDefault();
-            var url = prompt("Enter URL of Judge0 API:", apiUrl);
-            if (url != null) {
-                url = url.trim();
-            }
-            if (url != null && url != "") {
-                apiUrl = url;
-                localStorageSetItem("api-url", apiUrl);
-                showApiUrl();
-            }
-        } else if (keyCode == 118) { // F7
-            e.preventDefault();
-            wait = !wait;
-            localStorageSetItem("wait", wait);
-            alert(`Submission wait is ${wait ? "ON. Enjoy" : "OFF"}.`);
         } else if (event.ctrlKey && keyCode == 83) { // Ctrl+S
             e.preventDefault();
             save();
@@ -418,13 +310,14 @@ $(document).ready(function () {
         layout.registerComponent("source", function (container, state) {
             sourceEditor = monaco.editor.create(container.getElement()[0], {
                 automaticLayout: true,
-                theme: "vs-dark",
+                theme: "vs",
                 scrollBeyondLastLine: false,
                 readOnly: state.readOnly,
                 language: "cpp"
             });
 
             sourceEditor.getModel().onDidChangeContent(function (e) {
+                currentLanguageId = parseInt($selectLanguage.val());
                 isEditorDirty = sourceEditor.getValue() != sources[currentLanguageId];
             });
         });
@@ -432,7 +325,7 @@ $(document).ready(function () {
         layout.registerComponent("stdin", function (container, state) {
             stdinEditor = monaco.editor.create(container.getElement()[0], {
                 automaticLayout: true,
-                theme: "vs-dark",
+                theme: "vs",
                 scrollBeyondLastLine: false,
                 readOnly: state.readOnly,
                 language: "plaintext"
@@ -442,68 +335,10 @@ $(document).ready(function () {
         layout.registerComponent("stdout", function (container, state) {
             stdoutEditor = monaco.editor.create(container.getElement()[0], {
                 automaticLayout: true,
-                theme: "vs-dark",
+                theme: "vs",
                 scrollBeyondLastLine: false,
                 readOnly: state.readOnly,
                 language: "plaintext"
-            });
-
-            container.on("tab", function(tab) {
-                tab.element.append("<span id=\"stdout-dot\" class=\"dot\" hidden></span>");
-                tab.element.on("mousedown", function(e) {
-                    e.target.closest(".lm_tab").children[3].hidden = true;
-                });
-            });
-        });
-
-        layout.registerComponent("stderr", function (container, state) {
-            stderrEditor = monaco.editor.create(container.getElement()[0], {
-                automaticLayout: true,
-                theme: "vs-dark",
-                scrollBeyondLastLine: false,
-                readOnly: state.readOnly,
-                language: "plaintext"
-            });
-
-            container.on("tab", function(tab) {
-                tab.element.append("<span id=\"stderr-dot\" class=\"dot\" hidden></span>");
-                tab.element.on("mousedown", function(e) {
-                    e.target.closest(".lm_tab").children[3].hidden = true;
-                });
-            });
-        });
-
-        layout.registerComponent("compile output", function (container, state) {
-            compileOutputEditor = monaco.editor.create(container.getElement()[0], {
-                automaticLayout: true,
-                theme: "vs-dark",
-                scrollBeyondLastLine: false,
-                readOnly: state.readOnly,
-                language: "plaintext"
-            });
-
-            container.on("tab", function(tab) {
-                tab.element.append("<span id=\"compile-output-dot\" class=\"dot\" hidden></span>");
-                tab.element.on("mousedown", function(e) {
-                    e.target.closest(".lm_tab").children[3].hidden = true;
-                });
-            });
-        });
-
-        layout.registerComponent("sandbox message", function (container, state) {
-            sandboxMessageEditor = monaco.editor.create(container.getElement()[0], {
-                automaticLayout: true,
-                theme: "vs-dark",
-                scrollBeyondLastLine: false,
-                readOnly: state.readOnly,
-                language: "plaintext"
-            });
-
-            container.on("tab", function(tab) {
-                tab.element.append("<span id=\"sandbox-message-dot\" class=\"dot\" hidden></span>");
-                tab.element.on("mousedown", function(e) {
-                    e.target.closest(".lm_tab").children[3].hidden = true;
-                });
             });
         });
 
@@ -513,7 +348,6 @@ $(document).ready(function () {
             } else {
                 loadRandomLanguage();
             }
-            $("#site-navigation").css("border-bottom", "1px solid black");
         });
 
         layout.init();
@@ -521,177 +355,12 @@ $(document).ready(function () {
 });
 
 // Template Sources
-var bashSource = "echo \"hello, world\"\n";
-
-var basicSource = "PRINT \"hello, world\"\n";
-
-var cSource = "\
-#include <stdio.h>\n\
-\n\
-int main(void) {\n\
-    printf(\"hello, world\\n\");\n\
-    return 0;\n\
-}\n";
-
-var cppSource = "\
-#include <iostream>\n\
-\n\
-int main() {\n\
-    std::cout << \"hello, world\" << std::endl;\n\
-    return 0;\n\
-}\n";
-
-var csharpSource = "\
-public class Hello {\n\
-    public static void Main() {\n\
-        System.Console.WriteLine(\"hello, world\");\n\
-    }\n\
-}\n";
-
-var clojureSource = "(println \"hello, world\")\n";
-
-var crystalSource = "puts \"hello, world\"\n";
-
-var elixirSource = "IO.puts \"hello, world\"\n";
-
-var erlangSource = "\
-main(_) ->\n\
-    io:fwrite(\"hello, world\\n\").\n";
-
-var goSource = "\
-package main\n\
-\n\
-import \"fmt\"\n\
-\n\
-func main() {\n\
-    fmt.Println(\"hello, world\")\n\
-}\n";
-
-var haskellSource = "main = putStrLn \"hello, world\"\n";
-
-var insectSource = "\
-2 min + 30 s\n\
-40 kg * 9.8 m/s^2 * 150 cm\n\
-sin(30°)\n";
-
-var javaSource = "\
-public class Main {\n\
-    public static void main(String[] args) {\n\
-        System.out.println(\"hello, world\");\n\
-    }\n\
-}\n";
-
-var javaScriptSource = "console.log(\"hello, world\");\n";
-
-var ocamlSource = "print_endline \"hello, world\";;\n";
-
-var octaveSource = "printf(\"hello, world\\n\");\n";
-
-var pascalSource = "\
-program Hello;\n\
-begin\n\
-    writeln ('hello, world')\n\
-end.\n";
-
-var pythonSource = "print(\"hello, world\")\n";
-
-var rubySource = "puts \"hello, world\"\n";
-
-var rustSource = "\
-fn main() {\n\
-    println!(\"hello, world\");\n\
-}\n"
-
-var textSource = "hello, world\n";
+var pythonSource = "print(\"Bok Stemalica!\")\n";
 
 var sources = {
-    1: bashSource,
-    2: bashSource,
-    3: basicSource,
-    4: cSource,
-    5: cSource,
-    6: cSource,
-    7: cSource,
-    8: cSource,
-    9: cSource,
-    10: cppSource,
-    11: cppSource,
-    12: cppSource,
-    13: cppSource,
-    14: cppSource,
-    15: cppSource,
-    16: csharpSource,
-    17: csharpSource,
-    18: clojureSource,
-    19: crystalSource,
-    20: elixirSource,
-    21: erlangSource,
-    22: goSource,
-    23: haskellSource,
-    24: haskellSource,
-    25: insectSource,
-    26: javaSource,
-    27: javaSource,
-    28: javaSource,
-    29: javaScriptSource,
-    30: javaScriptSource,
-    31: ocamlSource,
-    32: octaveSource,
-    33: pascalSource,
-    34: pythonSource,
-    35: pythonSource,
-    36: pythonSource,
-    37: pythonSource,
-    38: rubySource,
-    39: rubySource,
-    40: rubySource,
-    41: rubySource,
-    42: rustSource,
-    43: textSource
+    34: pythonSource
 };
 
 var fileNames = {
-    1: "script.sh",
-    2: "script.sh",
-    3: "main.bas",
-    4: "main.c",
-    5: "main.c",
-    6: "main.c",
-    7: "main.c",
-    8: "main.c",
-    9: "main.c",
-    10: "main.cpp",
-    11: "main.cpp",
-    12: "main.cpp",
-    13: "main.cpp",
-    14: "main.cpp",
-    15: "main.cpp",
-    16: "Main.cs",
-    17: "Main.cs",
-    18: "main.clj",
-    19: "main.cr",
-    20: "main.exs",
-    21: "main.erl",
-    22: "main.go",
-    23: "main.hs",
-    24: "main.hs",
-    25: "main.ins",
-    26: "Main.java",
-    27: "Main.java",
-    28: "Main.java",
-    29: "main.js",
-    30: "main.js",
-    31: "main.ml",
-    32: "file.m",
-    33: "main.pas",
-    34: "main.py",
-    35: "main.py",
-    36: "main.py",
-    37: "main.py",
-    38: "main.rb",
-    39: "main.rb",
-    40: "main.rb",
-    41: "main.rb",
-    42: "main.rs",
-    43: "source.txt"
+    34: "main.py"
 };
